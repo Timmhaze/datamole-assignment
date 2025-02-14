b51c85918fb757b21a255887041ba81f6f55836b
Timothy Hayes <TimHayes95@gmail.com>
2025-02-14 12:47:03 +0100

    F2 complete

--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -1,15 +1,52 @@
+import React, {useEffect, useState} from "react";
 import styled from "styled-components";
 
-export const List = styled.div`
+const ListStyled = styled.div`
     display: flex;
     flex-direction: column;
 `;
 
-export const ListItem = () => {
+interface TodoItem {
+    label: string;
+    isDone: boolean;
+    createdAt: number;
+    id: number;
+}
+
+export const List = () => {
+    
+    const [items, setItems] = useState<TodoItem[]>([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/items") // URL for data
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Network response broke");
+            }
+            return response.json();
+          })
+          .then((data) => {
+            console.log("data objects:", data); 
+            setItems(data);
+          })
+          .catch((error) => {
+            console.error("There was a problem with getting the data:", error);
+          });
+      }, []);
 
     return (
-        <>
-        test
-        </>
+        <ListStyled>
+            {items.map((item) => (
+                <div key={item.id}>
+                    <h3>{item.label}</h3>
+                    <input
+                        type="checkbox"
+                        checked={item.isDone}
+                        readOnly
+                    />
+                    <hr /> {/* <----- Just for readability*/}
+                </div>
+            ))}
+        </ListStyled>
     );
 };
